dff1c6504980b10a8b8135de7bf38fa181f83955
Julli Mayanne <julli.mayanne@gruposbf.com.br>
2022-01-04 10:12:52 -0300

    entrada de dados

--- lib/campoTexto.dart
+++ lib/campoTexto.dart
@@ -10,6 +10,22 @@ class _CampoTextoState extends State<CampoTexto> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Entrada de dados"),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(32),
+            child: TextField(
+              keyboardType: TextInputType.text,
+
+            ),
+            ),
+
+
+        ],
+      ),
       
     );
   }
